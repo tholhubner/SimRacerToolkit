efc19daaf8b62853a6fd2f3ab7d342ba19095e82
Tyler Holhubner <tholhubner@Tylers-Mac-mini.local>
2023-04-03 05:26:06 -0400

    added GearList Screen skeleton

---
+++ src/screens/GearList.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { SafeAreaView } from "react-native"
+import { Text } from "@rneui/base"
+
+function GearList(): JSX.Element {
+  return (
+    <SafeAreaView>
+			<Text style={{ color: "black" }}>Valtteri, it's James</Text>
+		</SafeAreaView>
+  )
+}
+
+export { GearList }
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,1 +1,2 @@
+export * from "./GearList"
 export * from "./Landing"
